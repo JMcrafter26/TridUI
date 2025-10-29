42c2992c1bc8cfd02b64b414ff7cd250f896438b
JMcrafter26 <77780772+JMcrafter26@users.noreply.github.com>
2025-10-30 00:39:01 +0100

    Initial

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import Header from './Header.svelte';
+	import '../app.css';
+	import { setLocale } from '$lib/paraglide/runtime';
+	import { m } from '$lib/paraglide/messages.js';
+
+
+	let { children } = $props();
+</script>
+
+<div class="app">
+	<Header />
+ 
+	<main>
+		{@render children()}
+	</main>
+ 
+	<footer>
+		<p>&copy; 2025 TridscanUI</p>
+		<p>{m.made_by_footer()}</p>
+	</footer>
+</div>
---
+++ frontend/vite.config.ts
@@ -0,0 +1,18 @@
+import { paraglideVitePlugin } from '@inlang/paraglide-js';
+import tailwindcss from "@tailwindcss/vite";
+import { sveltekit } from '@sveltejs/kit/vite';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+	plugins: [
+		tailwindcss(),
+		sveltekit(),
+		paraglideVitePlugin({
+			project: './project.inlang',
+			// Write generated paraglide artifacts to a build folder instead of src
+			// This prevents the plugin from overwriting or replacing files inside `src/lib`
+			// (which can cause JSONs to be converted into generated JS files in-source).
+			outdir: './build/paraglide'
+		})
+	]
+});
